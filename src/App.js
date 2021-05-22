944d45efeb9888476f40b7265e6378a5c8c24a22
mariraja508 <82996325+mariraja508@users.noreply.github.com>
2021-05-22 18:10:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, memo, useCallback, useMemo, useRef, useEffect } from 'react';
-
+import ReactPlayer from 'react-player';
 export default function App(){
 return <div>
   <ReactPlayer controlsurl="blob:http://localhost:3000/34828824-06c8-466b-806f-915d95071124"/>
